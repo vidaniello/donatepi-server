8bc0f57fb7ce69e5a2023a1fbe8258438136deec
Glitch (glitch-hello-website) <none>
2021-07-01 10:32:13 +0000

    😚🏮 Checkpoint ./index.js:6330142/577

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ app.post('/', (req, res) => {
   res.send('nothing to do here!');
 });
 
+app.post('/printmyname', (req, res) => {
+  
+  let resp = 'your name: '+req.query.name+"\r\n";
+  resp += req.body;
+  
+  res.send(resp);
+});
+
 app.listen(process.env.PORT, () => {
   //console.log("Example app listening at http://localhost:"+process.env.PORT);
+  console.log("done");
 });
