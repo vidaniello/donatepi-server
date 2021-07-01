d094110cbc42a04b2d8ce67143aa2dd81b01eaac
Glitch (glitch-hello-website) <none>
2021-07-01 10:44:07 +0000

    🌕🎗 Checkpoint ./index.js:6330142/131 ./package.json:6330142/448

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+
 const app = express();
+
+var jsonParser = bodyParser.json();
 
 const piEndpoint = "https://api.minepi.com/v2";
 
 const somevar = process.env.SOMEVAR;
 
 app.get('/', (req, res) => {
-  
-  
   res.send('nothing to do here!');
 });
 
 app.post('/', (req, res) => {
-  
-  
   res.send('nothing to do here!');
 });
 
-app.post('/printmyname', (req, res) => {
+app.post('/printmyname', jsonParser, (req, res) => {
   
-  let resp = 'your name: '+req.query.name+"\r\n";
+  let resp = 'your name: '+req.query.name;
   resp += req.body;
   
   res.send(resp);
 });
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(process.env.PORT, () => {
   //console.log("Example app listening at http://localhost:"+process.env.PORT);
