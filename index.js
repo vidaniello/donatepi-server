d947a0d259e8c99aaff523bf387c767f18858226
Glitch (glitch-hello-website) <none>
2021-07-01 09:39:06 +0000

    🙅👽 Checkpoint ./index.js:6330142/83 ./package.json:6330142/29

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('nothing to do here!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
